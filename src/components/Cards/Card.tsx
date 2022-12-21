71f898aba722b710f8535cc9e57d51b800164db5
deepyjr <52158453+deepyjr@users.noreply.github.com>
2022-12-21 11:22:57 +0100

    update cards

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import './Cards.css'
 function Card() {
   return (
-    <div>Card</div>
+    <div className="container-card">
+        <div className="img-card">
+            <img src="https://picsum.photos/250/200" alt="" />
+        </div>
+        <h3>Rick Sanchez</h3>
+        <p>Specie : Human</p>
+        <p>Last location  : Citadel of Ricks</p>
+    </div>
   )
 }
 
--- src/pages/RickAndMortyPage.tsx
+++ src/pages/RickAndMortyPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Card from '../components/Cards';
-
+import './RickAndMortyPage.css'
 function RickAndMortyPage() {
   return (
     <>
+      <div className="container">
+        <div className="container-cards">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
       <p>RickAndMortyPage</p>
-      <Card></Card>
     </>
-  )
+  );
 }
 
 export default RickAndMortyPage
